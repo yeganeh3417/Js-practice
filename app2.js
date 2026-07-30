f8bf470c6c6b299e86fb04388a5ac00f33e221fe
yeganeh <yeganeh.nzrr@gmail.com>
2026-07-30 10:11:06 +0330

    new exercise

---
+++ app2.js
@@ -0,0 +1,33 @@
+// var arr = [2, 3, 5, 7, 8, 10, 12];
+
+// var maxEven = arr[0];
+// var minOdd = arr[1];
+
+// for(var i=0 ; i<arr.length ; i++) {
+//    if(arr[i] % 2 === 0) {
+//        if(arr[i]>maxEven) {
+//            maxEven = arr[i];
+//        }
+//    }
+//    if(arr[i] % 2 !== 0) {
+//        if(arr[i]<minOdd) {
+//            minOdd = arr[i];
+//        }
+//    }
+// }
+// console.log(maxEven);
+// console.log(minOdd);
+
+
+
+// var arr = [2, 10, 5, 2, 3];
+
+// for (var i = 0 ; i<arr.length ; i++) {
+//    var sum = 0;
+//    for (var j = 0 ; j<arr.length ; j++) {
+//        if (arr[i] === arr[j]) {
+//            sum = sum + 1;
+//        }
+//    }
+//    console.log(arr[i] + " -> " + sum);
+// }
