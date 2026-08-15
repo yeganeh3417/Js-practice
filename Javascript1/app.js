dde47147dacc48fd1655934a6f54270afb64b6fc
yeganeh <yeganeh.nzrr@gmail.com>
2026-08-15 11:46:48 +0330

    new exercise and update Js-exercise

--- Javascript1/app.js
+++ Javascript1/app.js
@@ -1,32 +1,73 @@
-var li =
-document.getElementsByTagName("li");
-for (var i=0 ; i<li.length ; i++) {
-    li[i].innerText = (i+1) + ". "
-    +li[i].innerText;
+// var li =
+// document.getElementsByTagName("li");
+// for (var i=0 ; i<li.length ; i++) {
+//    li[i].innerText = (i+1) + ". "
+//    +li[i].innerText;
 
-    if((i+1) %2 === 0) {
-        li[i].style.color = "red";
-    } else {
-        li[i].style.color = "green";
+//    if((i+1) %2 === 0) {
+//        li[i].style.color = "red";
+//    } else {
+//        li[i].style.color = "green";
+//    }
+// }
+
+
+
+// var img = document.getElementsByTagName("img");
+// img[0].setAttribute("src", "laptop.webp");
+// img[0].setAttribute("alt", "laptop");
+// img[0].setAttribute("width", "200");
+// img[0].setAttribute("height", "200");
+
+// img[0].style.border = "2px solid gray";
+// img[0].style.borderRadius = "20px";
+
+
+
+// var div = document.getElementById("mydiv");
+// div.innerHTML= `
+// <p>yeganeh</p>
+// <span>age: 17</span>
+// <img src="./laptop.webp" width="100">
+// `;
+
+
+
+var text = document.getElementById("count");
+console.log(typeof text.innerText);
+
+var number = Number(text.innerText);
+console.log(typeof number);
+checkStyle();
+
+function handleDecrease() {
+    if(number > 0) {
+       number--;
+       text.innerText = number;
     }
+    checkStyle()
+}
+function handleIncrease() {
+    if(number < 5) {
+       number++;
+       text.innerText = number;
+    }
+    checkStyle()
 }
 
+function checkStyle() {
+    var incBtn = document.getElementById("inc");
+    var decBtn = document.getElementById("dec");
 
+    if(number >= 5) {
+        incBtn.setAttribute("disabled", true)
+    }else{
+        incBtn.removeAttribute("disabled")
+    }
 
-var img = document.getElementsByTagName("img");
-img[0].setAttribute("src", "laptop.webp");
-img[0].setAttribute("alt", "laptop");
-img[0].setAttribute("width", "200");
-img[0].setAttribute("height", "200");
-
-img[0].style.border = "2px solid gray";
-img[0].style.borderRadius = "20px";
-
-
-
-var div = document.getElementById("mydiv");
-div.innerHTML= `
-<p>yeganeh</p>
-<span>age: 17</span>
-<img src="./laptop.webp" width="100">
-`;
+    if(number <= 0) {
+        decBtn.setAttribute("disabled", true)
+    }else{
+        decBtn.removeAttribute("disabled")
+    }
+}
---
+++ Product/app.js
@@ -0,0 +1,74 @@
+var products = [
+    {
+        name: "Laptop",
+        category: "Electronics",
+        price: 1000,
+        quantity: 10
+    },
+    {
+        name: "Shoes",
+        category: "Fashion",
+        price: 80,
+        quantity: 15
+    },
+    {
+        name: "Phone",
+        category: "Electronics",
+        price: 600,
+        quantity: 25
+    },
+    {
+        name: "iphone 17",
+        category: "Mobile",
+        price: 1000,
+        quantity: 5
+    }
+];
+
+var list = document.getElementById("productList");
+
+function showProducts() {
+    list.innerHTML = "";
+    var search = document.getElementById("search").value;
+    var category = document.getElementById("categoryFilter").value;
+
+    for (var i=0 ; i<products.length ; i++) {
+        if (products[i] != null &&
+             (search == "" || products[i].name == search) &&
+             (category == "All" || products[i].category == category)
+            ) {
+
+                 list.innerHTML +=
+                 "<tr>" +
+                 "<td>" + products[i].name + "</td>" +
+                 "<td>" + products[i].category + "</td>" +
+                 "<td>" + products[i].price + "</td>" +
+                 "<td>" + products[i].quantity + "</td>" +
+                 "<td><button onclick='deleteProduct("+ i +")'>Delete</button></td>" +
+                 "</tr>";
+             }
+    }
+}
+function deleteProduct(i) {
+    products[i] = null;
+    showProducts();
+}
+
+function addProduct() {
+
+    products[products.length] = {
+        name: document.getElementById("name").value,
+        category: document.getElementById("category").value,
+        price: document.getElementById("price").value,
+        quantity: document.getElementById("quantity").value
+    };
+    document.getElementById("name").value = "";
+    document.getElementById("category").value = "";
+    document.getElementById("price").value = "";
+    document.getElementById("quantity").value = "";
+
+    showProducts();
+}
+
+showProducts();
+
